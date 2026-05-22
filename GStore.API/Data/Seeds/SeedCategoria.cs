33702e9155daab190164a36e54c19f303afcc3c3
gallojunior <gallojunior@gmail.com>
2026-05-22 12:11:12 -0300

    Mock de dados

--- GStore.API/Data/Seeds/SeedCategoria.cs
+++ GStore.API/Data/Seeds/SeedCategoria.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GStore.API.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace GStore.API.Data.Seeds
+namespace GStore.API.Data.Seeds;
+
+public class SeedCategoria
 {
-    public class SeedCategoria
+    public SeedCategoria(ModelBuilder modelBuilder)
     {
-        
+        List<Categoria> categorias = [
+            new() { Id = 1, Nome = "Smartphones" },
+            new() { Id = 2, Nome = "Notebooks" },
+        ];
+        modelBuilder.Entity<Categoria>().HasData(categorias);
     }
 }
--- GStore.API/Data/Seeds/SeedProduto.cs
+++ GStore.API/Data/Seeds/SeedProduto.cs
@@ -1,12 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GStore.API.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace GStore.API.Data.Seeds
+namespace GStore.API.Data.Seeds;
+
+public class SeedProduto
 {
-    public class SeedProduto
+    public SeedProduto(ModelBuilder modelBuilder)
     {
-        
+        List<Produto> produtos = [
+            // Smarthphone = 1
+            new() {
+                Id = 1,
+                CategoriaId = 1,
+                Nome = "",
+                Descricao = @"",
+                ValorCusto = 1,
+                ValorVenda = 1.99m,
+                Qtde = 0,
+                Destaque = true,
+                Foto = "/img/produtos/1.png"
+            },
+        ];
+        modelBuilder.Entity<Produto>().HasData(produtos);
     }
 }
